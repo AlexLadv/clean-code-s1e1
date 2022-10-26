7c6af53e0858f1d54962e399a00308a75b0824c7
Alex <cuba-1@mail.ru>
2022-10-26 16:26:57 +0500

    refactor: rule 1.2 Alternative for multimedia from html-and-css-extended is met

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="delete button";
     deleteButtonImg.className="delete-button-img";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.alt='';
     deleteButton.appendChild(deleteButtonImg);
 
 
